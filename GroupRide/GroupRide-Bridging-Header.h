75f304d3eb649bbb73e8cda38d7cd4919b591985
Krysta Deluca <krysta.arwen@gmail.com>
2018-08-09 17:02:55 -0700

    Add firebase.h and bridging header

---
+++ GroupRide/GroupRide-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  GroupRide-Bridging-Header .h
+//  GroupRide
+//
+//  Created by Krysta Deluca on 8/9/18.
+//  Copyright © 2018 Krysta Deluca. All rights reserved.
+//
+
+#ifndef GroupRide_Bridging_Header__h
+#define GroupRide_Bridging_Header__h
+
+#import "Firebase.h"
+
+#endif /* GroupRide_Bridging_Header__h */
